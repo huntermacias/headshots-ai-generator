a8f7f3062ba8df3bf5ca856ebe02bba75a830dda
Hunter Macias <huntermacias@Hunters-MacBook-Pro.local>
2024-02-15 21:14:30 -0800

    updated homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,42 @@ import "./globals.css";
 import { Suspense } from "react";
 
 export const metadata = {
-  title: "Headshots AI",
-  description: "Generate awesome headshots in minutes using AI",
+  title: "Elite Portraits | AI-Driven Headshot Revolution",
+  description: "Craft your professional legacy with Elite Portraits. Leveraging advanced AI, we transform your digital presence with stunning headshots in mere moments—trusted by Fortune 500 leaders.",
 };
+
 
 export default function RootLayout({ children }:any) {
   return (
     <>
       <head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        {/* Include any additional meta tags, fonts, or CSS links here */}
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" rel="stylesheet" />
-      </head>
+  <title>{metadata.title}</title>
+  <meta name="description" content={metadata.description} />
+
+  {/* Open Graph / Facebook Meta Tags */}
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content={metadata.title} />
+  <meta property="og:description" content={metadata.description} />
+  <meta property="og:image" content="/public/hero2.webp" /> {/* Replace with the path to your image */}
+  <meta property="og:url" content="https://headshothub.vercel.app/" /> {/* Replace with your domain */}
+  <meta property="og:site_name" content="Headshots AI" />
+
+  {/* Twitter Card Meta Tags */}
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content={metadata.title} />
+  <meta name="twitter:description" content={metadata.description} />
+  <meta name="twitter:image" content="/public/hero2.webp" /> {/* Replace with the path to your Twitter-specific image */}
+  <meta name="twitter:site" content="@huntermacias_" /> {/* Replace with your Twitter handle */}
+  <meta name="twitter:creator" content="@huntermacias_" /> {/* Replace with your Twitter handle */}
+
+  {/* LinkedIn does not use specific meta tags, it will utilize Open Graph tags */}
+
+  {/* Additional meta tags */}
+  <link rel="canonical" href="https://headshothub.vercel.app/" /> {/* Replace with your domain */}
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;800&display=swap" rel="stylesheet" />
+</head>
+
       <body className="min-h-screen flex flex-col bg-[#121212] text-[#E0E0E0] font-inter">
         <section className="w-full">
           <Suspense fallback={<div>Loading...</div>}>
