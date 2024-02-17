dd71063ab422c5d392c29bbe05eee967539a3cce
Hunter Macias <huntermacias@Hunters-MacBook-Pro.local>
2024-02-17 10:48:09 -0800

    updated login label issue

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -14,43 +14,44 @@ export const dynamic = "force-dynamic";
 
 export default async function Login() {
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-12 px-4 sm:px-6 lg:px-8">
       <form
-        className="flex-1 flex flex-col w-full justify-center gap-2 "
+        className="w-full max-w-md space-y-8"
         action="/auth/sign-in"
         method="post"
       >
-        <Card>
+        <Card className="overflow-hidden shadow-md">
           <CardHeader>
             <CardTitle>Log In / Sign Up</CardTitle>
             <CardDescription>
-              Log into your account or sign up for a new one to get started.
+              Log into your account or sign up to get started.
             </CardDescription>
           </CardHeader>
-          <CardContent className="grid gap-4">
-            <Label className="text-md" htmlFor="email">
-              Email
-            </Label>
-            <input
-              className="rounded-md px-4 py-2 bg-inherit border"
-              name="email"
-              placeholder="you@example.com"
-              required
-            />
-            <Button>Continue</Button>
+          <CardContent className="space-y-6">
+            <div>
+              <Label htmlFor="email">Email Address</Label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                placeholder="you@example.com"
+                required
+              />
+            </div>
+            <Button className="w-full">Continue</Button>
             <Messages />
           </CardContent>
           <CardFooter>
-            <p className="text-sm">
-              By signing up, you agree to our{" "}
-              <a href="#" className="underline">
+            <p className="text-sm text-center">
+              By signing up, you agree to our
+              <a href="#" className="mx-1 underline hover:text-indigo-500">
                 Terms of Service
-              </a>{" "}
-              and{" "}
-              <a href="#" className="underline">
-                Privacy Policy
               </a>
-              .
+              and
+              <a href="#" className="mx-1 underline hover:text-indigo-500">
+                Privacy Policy
+              </a>.
             </p>
           </CardFooter>
         </Card>
