ff2222c0f6f7aae9d99be41989df62adf13119b0
Hunter Macias <huntermacias@Hunters-MacBook-Pro.local>
2024-02-25 17:20:32 -0800

    fixed build dynamic render issue

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ import { Button } from "@/components/ui/button";
 import ExplainerSection from "@/components/ExplainerSection";
 import PricingSection from "@/components/PricingSection";
 import Image from "next/image";
+import GalleryCard from "@/components/galleryCard";
+import { imageData } from "@/lib/galleryData";
 
 export const dynamic = "force-dynamic";
 
@@ -176,6 +178,19 @@ export default async function Index() {
 
 
       <ExplainerSection />
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+      {Object.entries(imageData.images).map(([key, urls], index) => (
+        <GalleryCard
+          key={index}
+          title={`${key.replace('-', ' ').toUpperCase()} SETS`}
+          description={`Description for ${key}`}
+          numPhotos={urls.length}
+          timesSelected={`${Math.floor(Math.random() * (55 - 4 + 1) + 4)}`} // Example usage, replace with actual data
+          imageUrls={urls}
+          slug={`pack=${key}`}
+        />
+      ))}
+    </div>
       <PricingSection />
 
     </div>
