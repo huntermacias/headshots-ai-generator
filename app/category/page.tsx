f8d3c09977c4af821ef70b4119ed6712fb0813c3
Hunter Macias <huntermacias@Hunters-MBP.lan>
2024-03-05 00:13:29 -0600

    updated gallery card design

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -167,19 +167,7 @@ const CategoryPage = () => {
           ))}
         </div>
 
-        <div className='mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
-      {Object.entries(imageData.images).slice(0,4).map(([key, value], index) => (
-        <GalleryCard
-          key={key} // Using `key` as the react key for better identity management
-          title={`${key.replaceAll('_', ' ').toUpperCase()} COLLECTION`}
-          description={value.description}
-          numPhotos={value.urls.length}
-          timesSelected={`${index + 1 + Math.floor(Math.random() * (52))}`} // Simplified random logic
-          imageUrls={value} // Directly passing the `value` object which should match expected shape
-          slug={key}
-        />
-      ))}
-    </div>
+
       </div>
     </div>
 
